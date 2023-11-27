a1d2fd34d6c5394ff6a8405371556cfb70af5a1c
Lzbdc <150091473+Lzbdc@users.noreply.github.com>
2023-11-27 18:45:17 -0300

    Add files via upload

---
+++ projeto.js
@@ -0,0 +1,22 @@
+const form = document.getElementById("center")
+
+function validaB(campoA, campoB) {
+    return campoB > campoA;
+}
+form.addEventListener('submit',function(e){
+    e.preventDefault();
+    let campoA = parseInt(document.getElementById('n1').value)
+    let campoB = parseInt(document.getElementById('n2').value)
+    
+    if (validaB(campoA, campoB)) {
+        document.querySelector('.error-message').style.display = 'none';
+        document.querySelector('.success-message').style.display = 'block';
+
+        campoA.value = '';
+    }
+    else{
+        document.querySelector('.error-message').style.display = 'block';
+        document.querySelector('.success-message').style.display = 'none';
+    }
+})
+
